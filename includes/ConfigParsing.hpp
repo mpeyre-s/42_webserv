2966e0d7962e842cf22074177ee42d03c47f0ba8
Arnaud Fontan <afontan@c2r6p6.42perpignan.fr>
2025-06-10 16:10:10 +0200

    Work PARSING, make serverBlock

--- includes/ConfigParsing.hpp
+++ includes/ConfigParsing.hpp
@@ -2,13 +2,16 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 #include "Server.hpp"
 
 class ConfigParsing {
 private:
-	std::vector<Server*> _list_servers;
+	std::vector<Server*> 					_list_servers;
+	// std::vector<std::string>				_serverBlock;
+	std::vector<std::vector<std::string> >	_serverList;
 
 public:
 	ConfigParsing(std::string &configFile);
--- srcs/ConfigParsing.cpp
+++ srcs/ConfigParsing.cpp
@@ -1,8 +1,44 @@
 #include "../includes/ConfigParsing.hpp"
 
-ConfigParsing::ConfigParsing(std::string &configFile) {
-	(void)configFile;
-	_list_servers.push_back(new Server());
+ConfigParsing::ConfigParsing(std::string &configFile) 
+{
+	std::ifstream cfFile(configFile.c_str());
+	if (!cfFile)
+	{
+		std::cerr << "Cannot open config file";
+		return;
+	}
+	std::string line;
+	int bracket = 0;
+	bool insideServerBlock;
+	std::vector<std::string> serverBlock;
+	while (getline(cfFile, line))
+	{
+		if (line.find("server {") != std::string::npos)
+		{
+			//bracket++;
+			insideServerBlock = true;
+		}
+		if (line.find('{') != std::string::npos)
+			bracket++;
+		if (line.find('}') != std::string::npos)
+			bracket--;
+		serverBlock.push_back(line);
+		if (bracket == 0)
+		{
+			_serverList.push_back(serverBlock);
+			//serverBlock.clear();
+			break;
+		}
+		std::cout << bracket << std::endl;
+	}
+	for(std::vector<std::string>::iterator it = serverBlock.begin(); it != serverBlock.end(); it++)
+		std::cout << *it << std::endl;
+	// for(std::vector<std::vector<std::string> >::iterator it1 = _serverList.begin(); it1 != _serverList.end(); it1++)
+	// {
+
+	// }
+
 }
 
 ConfigParsing::~ConfigParsing() {}
